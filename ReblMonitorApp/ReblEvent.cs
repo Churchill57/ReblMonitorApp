9fc7ae0885c1ea06603c71c89b875837a3af97c6
John Hutson <John.Hutson@justretirement.com>
2017-12-12 10:11:30 +0000

    Change colour property to string

--- ReblMonitorApp/ReblEvent.cs
+++ ReblMonitorApp/ReblEvent.cs
@@ -10,7 +10,7 @@ namespace ReblMonitorApp
     {
         public string BusinessProcess { get; set; }
         public string StatusMessage { get; set; }
-        public int DisplayColour { get; set; }
+        public string DisplayColour { get; set; }
         public DateTime When { get; set; }
     }
 }
--- ReblMonitorApp/ReblEventStore.cs
+++ ReblMonitorApp/ReblEventStore.cs
@@ -14,7 +14,7 @@ namespace ReblMonitorApp
 
         public string BusinessProcess { get; set; }
         public string StatusMessage { get; set; }
-        public int DisplayColour { get; set; }
+        public string DisplayColour { get; set; }
         public DateTime When { get; set; }
 
     }
